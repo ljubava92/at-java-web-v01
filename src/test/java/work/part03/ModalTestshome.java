eb286475f575b27a0219d1d1694df351dc3afa88
ljubava92 <lubava92@mail.ru>
2025-12-17 22:18:17 +0300

    ДЗ АТ-1 сделано

--- src/test/java/work/part03/ModalTestshome.java
+++ src/test/java/work/part03/ModalTestshome.java
@@ -1,46 +1,39 @@
 package work.part03;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Condition.clickable;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class ModalTestshome {
-    @Test
-    void test01SimpleModal() {
-        Configuration.pageLoadStrategy = "eager";
-        open("https://practice-automation.com/modals/");
-        $x("//div[@id='popmake-1318']").shouldBe(exist);
-        $("#simpleModal").click();
-        sleep(2_000);
-        $x("//a[text()='Home']").click();
-        $x("//div[@id='popmake-1318']").shouldBe(visible);
-        $x("//div[@id='popmake-1318']//button").shouldBe(clickable);
-        $x("//div[@id='popmake-1318']//button").click();
-        sleep(2_000);
-    }
+//Домашнее задание AT-1 (обязательное): На сайте https://www.specialist.ru/ в модальном окне "Наш сайт использует файлы cookie" нажать "Согласен". Выбрать пункт меню "Форматы обучения", затем "Свободное обучение", затем "Выбрать курс", в поле "Направление" выбрать "Программирование", нажать кнопку "Применить" и убедиться, что на странице есть элемент содержащий текст "Тестирование ПО"
     @Test
     void test01Modal() {
         Configuration.pageLoadStrategy = "eager";
         open("https://www.specialist.ru/");
         getWebDriver().manage().window().maximize();
         $x("//div[@id='cookieConsent']").shouldBe(exist);
-        $("#cookieConsent__ok").click();
-        sleep(2_000);
+        $x("//*[@id='cookieConsent__ok']").click();
+        sleep(1_000);
         $x("//li[@class='header-menu-item']/a[text()='Форматы обучения']").click();
-        sleep(2_000);
-        $x("//div[@class='format-article-content']/a[text()='Свободное обучение']").click();
-    }
-    @Test
-    void test02Modal() {
-        Configuration.pageLoadStrategy = "eager";
-        open("https://www.specialist.ru/");
-    $x("//li[@class='header-menu-item']/a[text()='Форматы обучения']").shouldBe(clickable);
+        sleep(1_000);
+        $x("//a[contains(text(), 'Свободное обучение')]").click();
+        sleep(1_000);
+        $x("//a[@class='page-button banner-button' and @href='#selectCourse' and text()='Выбрать курс']").click();
+        sleep(1_000);
+        $x("//select[@id='Filter_CategoriesDirectionFilter']//option[@value='ПРГ']").click();
+        sleep(1_000);
+        $x("//button[@id='sendBtn']").click();
+        sleep(1_000);
+        $x("//a[@class='course-link' and contains(@href, 'course/tpo') and contains(text(), 'Тестирование ПО')]")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Тестирование ПО"), Duration.ofSeconds(10));
+        sleep(1_000);
 
     }
+
 }
